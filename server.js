2e4d3272de0105ee41efc18b8a4a9f2ebea7a2c2
horsemanjackyliu <jacky.liu02@sap.com>
2024-08-06 14:17:54 +0800

    change on 2024

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.use(express.text())
 app.use(bodyParser.json());
 
 function getSupEmail(situBody,jwtB){
-    const supId = situBody.I_POSITNCONFQTYDEFICIT.SUPPLIER;
+    // const supId = situBody.I_POSITNCONFQTYDEFICIT.SUPPLIER;
+    const supId = situBody.SUPPLIER;
+    
     console.log(supId);
     const { businessPartnerApi,businessPartnerAddressApi,addressEmailAddressApi} = businessPartnerService();
    return businessPartnerApi.requestBuilder()
@@ -45,9 +47,13 @@ app.post('/action', passport.authenticate('JWT', {session: false}), (req, res) =
         res.status(403).end('Forbidden. Authorization for webhook access is missing.')
     }else{
         //  console.log(req.body);
-         console.log(req.body.input.dataContext);
+        //  console.log(req.body.input.dataContext);
+        console.log(req.body);
+        console.log(typeof(req.body));
 
-         const situBody = JSON.parse(req.body.input.dataContext);
+        const situBody = (typeof(req.body)==='object') ? req.body: JSON.parse(req.body);
+
+        //  const situBody = JSON.parse(req.body);
 
          const jwtB = retrieveJwt(req);
 
@@ -57,18 +63,18 @@ app.post('/action', passport.authenticate('JWT', {session: false}), (req, res) =
                 service: 'Outlook365',
                 auth:{
                     user: 'firstname.lastname@outlook.com',
-                    pass: 'password'
+                    pass: 'emailPassword'
                 }
                 });
-                const content =  "There are deficit in purchase order item" + situBody.I_POSITNCONFQTYDEFICIT.PURCHASINGDOCUMENT + "-"
-                + situBody.I_POSITNCONFQTYDEFICIT.PURCHASINGDOCUMENTITEM + ",We Ordered " + situBody.I_POSITNCONFQTYDEFICIT.MATERIALNAME + "quantity"
-                 + situBody.I_POSITNCONFQTYDEFICIT.ORDEREDQUANTITY + " " + situBody.I_POSITNCONFQTYDEFICIT.PURCHASEORDERQUANTITYUNIT + ",but you only confirmed  "
-                 +  situBody.I_POSITNCONFQTYDEFICIT.CONFIRMEDQUANTITY + ", Would you please check again? ";
-                const subject = "deficit in Purchase Order " + situBody.I_POSITNCONFQTYDEFICIT.PURCHASINGDOCUMENT + "Item " + situBody.I_POSITNCONFQTYDEFICIT.PURCHASINGDOCUMENTITEM;
-
+                const content =  "There are deficit in purchase order item" + situBody.PURCHASINGDOCUMENT + "-"
+                + situBody.PURCHASINGDOCUMENTITEM + ",We Ordered " + situBody.MATERIALNAME + "quantity"
+                 + situBody.ORDEREDQUANTITY + " " + situBody.PURCHASEORDERQUANTITYUNIT + ",but you only confirmed  "
+                 +  situBody.CONFIRMEDQUANTITY + ", Would you please check again? ";
+                const subject = "deficit in Purchase Order " + situBody.PURCHASINGDOCUMENT + "Item " + situBody.PURCHASINGDOCUMENTITEM;
+console.log(email.toBusinessPartnerAddress[0].toEmailAddress[0].emailAddress );
 
             const mailoptions = {
-                from: "firstname.lastname@outlook.com",
+                from: "zhiwei.liu02@outlook.com",
                 to:  email.toBusinessPartnerAddress[0].toEmailAddress[0].emailAddress ,
                 subject: subject,
                 text:  content
